d884843060d84204ab5e331d1cd7dc99a1ded468
evgar <eugene.garichev@gmail.com>
2017-03-06 15:37:13 +0200

    add styles for tablet resolution

--- js/main.js
+++ js/main.js
@@ -2,11 +2,20 @@ $(document).ready(function () {
     $('.slider').slick({
         dots: true,
         fade: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
+        // autoplay: true,
+        // autoplaySpeed: 5000,
         speed: 1000,
         responsive: true,
         prevArrow: '.slider__prev-button',
-        nextArrow: '.slider__next-button'
+        nextArrow: '.slider__next-button',
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    dots: false
+                }
+            },
+        ]
     });
 });
